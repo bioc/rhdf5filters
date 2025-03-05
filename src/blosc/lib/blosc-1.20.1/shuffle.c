d3ed984756f3206f5c2c93dfc4dc1f8a7afdb5c6
Mike Smith <grimbough@gmail.com>
2025-03-05 10:32:40 +0100

    Patch blosc for C23

--- src/blosc/lib/blosc-1.20.1/shuffle.c
+++ src/blosc/lib/blosc-1.20.1/shuffle.c
@@ -21,12 +21,14 @@
 //#endif
 
 /* Visual Studio < 2013 does not have stdbool.h so here it is a replacement: */
-#if defined __STDC__ && defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
-/* have a C99 compiler */
-typedef _Bool bool;
+#if defined __STDC__ && defined __STDC_VERSION__
+  #if __STDC_VERSION__ >= 199901L && __STDC_VERSION__ <= 201710L 
+    /* have a C99 compiler */
+    typedef _Bool bool;
+  #endif
 #else
-/* do not have a C99 compiler */
-typedef unsigned char bool;
+  /* do not have a C99 compiler */
+  typedef unsigned char bool;
 #endif
 
 
